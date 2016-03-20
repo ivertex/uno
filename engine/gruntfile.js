63c2ebca75cc0a77cd10b634730031c1ce5b156c
ivertex <ivertex@gmail.com>
2016-03-20 03:07:53 +0300

    Change library header

--- engine/gruntfile.js
+++ engine/gruntfile.js
@@ -58,8 +58,8 @@ module.exports = function(grunt) {
         banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
         '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
         '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>\n' +
+        ' License <%= pkg.license %> */\n',
         // Task configuration.
         concat: {
             options: {
